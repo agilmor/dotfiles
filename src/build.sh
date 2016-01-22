6406bc744f41b4e1c3042644b25381090a9db31e
Albert Gil <agilmor@gmail.com>
2016-01-22 14:06:23 +0000

    - adding tmux sources as submodule

--- src/build.sh
+++ src/build.sh
@@ -1,3 +1,12 @@
+install_dir=$HOME/usr
 cd vim
-./configure --prefix=$HOME/usr --enable-luainterp=yes --enable-perlinterp=yes --enable-pythoninterp=yes --enable-python3interp=yes --enable-tclinterp=yes --enable-rubyinterp=yes --enable-cscope 
-make && make install
+./configure --prefix=$install_dir --enable-luainterp=yes     \
+                                  --enable-perlinterp=yes    \
+                                  --enable-pythoninterp=yes  \
+                                  --enable-python3interp=yes \
+                                  --enable-tclinterp=yes     \
+                                  --enable-rubyinterp=yes    \
+                                  --enable-cscope            && make && make install
+
+cd ../tmux
+./configure --prefix=$install_dir && make && make install
