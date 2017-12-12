2dcfd3c184b3652b87ca07d73c503fd2571fdbbc
Albert Gil <agilmor@gmail.com>
2017-12-12 10:47:56 +0100

    Renaming monitors

---
+++ .screenlayout/3monitors.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output VGA-0 --primary --mode 1600x1200 --pos 408x0 --rotate normal --output DVI-D-0 --mode 1280x1024 --pos 1280x1200 --rotate normal --output HDMI-0 --mode 1280x1024 --pos 0x1200 --rotate normal
