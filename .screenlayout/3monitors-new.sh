6a98060560813579328dfe819e1b41aa89483d99
Albert Gil <agilmor@gmail.com>
2018-12-14 17:13:03 +0100

    Addapted to bigger screens

--- .screenlayout/3monitors-new.sh
+++ .screenlayout/3monitors-new.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output VGA-0 --primary --mode 1920x1080 --pos 208x0 --rotate normal --output DVI-D-0 --mode 1280x1024 --pos 0x1080 --rotate normal --output HDMI-0 --mode 1280x1024 --pos 1280x1080 --rotate normal
+xrandr --output VGA-0 --primary --mode 1920x1080 --pos 120x0 --rotate normal --output DVI-D-0 --mode 1920x1080 --pos 0x1080 --rotate normal --output HDMI-0 --mode 1920x1080 --pos 1920x1080 --rotate normal
