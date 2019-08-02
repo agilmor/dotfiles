da5a0ef3bb3bbdb3ba508252ecff8f4a64e0a8e1
Albert Gil <agilmor@gmail.com>
2019-08-02 14:52:46 +0200

    Add swap to monitor chrome

--- cgroups/monitor-cgroups.sh
+++ cgroups/monitor-cgroups.sh
@@ -1,6 +1,11 @@
 chrome_tasks=`cat /sys/fs/cgroup/memory/chrome/agil/tasks | wc -l`
 chrome_limit=`cat /sys/fs/cgroup/memory/chrome/agil/memory.limit_in_bytes`
 chrome_usage=`cat /sys/fs/cgroup/memory/chrome/agil/memory.usage_in_bytes`
+chrome_limit_sw=`cat /sys/fs/cgroup/memory/chrome/agil/memory.memsw.limit_in_bytes`
+chrome_usage_sw=`cat /sys/fs/cgroup/memory/chrome/agil/memory.memsw.usage_in_bytes
+`
 printf 'Chrome Tasks    : %10s\n' $chrome_tasks
 printf 'Chrome Mem Limit: %10s\n' $chrome_limit
 printf 'Chrome Mem Usage: %10s\n' $chrome_usage
+printf 'Chrome Swp Limit: %10s\n' $chrome_limit_sw
+printf 'Chrome Swp Usage: %10s\n' $chrome_usage_sw
