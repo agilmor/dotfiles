b8e08ca67997690fb10d570b226bd339c3c59c1a
Albert Gil <albert.gil@schedmd.com>
2020-02-19 11:26:55 +0100

    Add cscope

--- src/build.sh
+++ src/build.sh
@@ -1,5 +1,5 @@
 #  sudo apt-get build-dep vim-gtk
-#  sudo apt-get install python-dev gcc g++ make automake cmake scons build-essential libncurses5-dev libevent-dev libpango1.0-dev xclip compton golang
+#  sudo apt-get install python-dev gcc g++ make automake cmake scons build-essential libncurses5-dev libevent-dev libpango1.0-dev xclip compton golang cscope
 #  sudo apt-get install libncurses5-dev libgnome2-dev libgnomeui-dev    libgtk2.0-dev libatk1.0-dev libbonoboui2-dev    libcairo2-dev libx11-dev libxpm-dev libxt-dev # for +clientserver of vim
 os_filename="/etc/os-release"
 if [ -f $os_filename ]; then
