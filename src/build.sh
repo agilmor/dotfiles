41ef54816e78308efb2c3a74e428234e8e106871
Albert Gil <albert.gil@happycomputing.cat>
2021-08-11 14:56:20 +0200

    Add bear

--- src/build.sh
+++ src/build.sh
@@ -1,5 +1,5 @@
 #  sudo apt-get build-dep vim-gtk
-#  sudo apt-get install python-dev gcc g++ make automake cmake scons build-essential libncurses5-dev libevent-dev libpango1.0-dev  golang cscope byacc libclang-12-dev
+#  sudo apt-get install python-dev gcc g++ make automake cmake scons bear build-essential libncurses5-dev libevent-dev libpango1.0-dev  golang cscope byacc libclang-12-dev
 #  sudo apt-get install libncurses5-dev libgnome2-dev libgnomeui-dev    libgtk2.0-dev libatk1.0-dev libbonoboui2-dev    libcairo2-dev libx11-dev libxpm-dev libxt-dev # for +clientserver of vim
 #  sudo apt-get install compton xclip unclutter numlockx
 #  sudo gem install tmuxinator
