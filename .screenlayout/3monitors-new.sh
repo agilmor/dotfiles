b8695240de772792585b8b3b4dc985aaf40d746c
Albert Gil <albert.gil@happycomputing.cat>
2023-08-20 11:55:50 +0200

    Clean screenlayout setup

--- .screenlayout/3monitors-new.sh
+++ .screenlayout/3monitors-new.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
-xrandr --output VGA-0 --mode 1920x1080 --pos 0x0 --rotate normal --output DVI-D-0 --off --output HDMI-1-2 --off --output HDMI-1-1 --primary --mode 1920x1080 --pos 0x1080 --rotate normal --output HDMI-0 --mode 1920x1080 --pos 1920x1080 --rotate normal --output DP-1-1 --off
+xrandr --output HDMI-0   --mode 1920x1080 --pos 1920x1080 --rotate normal --primary \
+       --output HDMI-1-1 --mode 1920x1080 --pos 0x1080    --rotate normal \
+       --output VGA-0    --mode 1920x1080 --pos 0x0       --rotate normal \
+       --output HDMI-1-2 --off \
+       --output DVI-D-0  --off \
+       --output DP-1-1   --off
