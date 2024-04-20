1e7bdbe119f3be95c1bfde469ca1e3765efb9ce8
Albert Gil <albert.gil@happycomputing.cat>
2024-04-20 10:14:48 +0200

    Adapt xrandr commands using --auto

--- .screenlayout/3monitors-new.sh
+++ .screenlayout/3monitors-new.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-xrandr --output HDMI-0   --mode 1920x1080 --pos 1920x1080 --rotate normal --primary \
-       --output HDMI-1-1 --mode 1920x1080 --pos 0x1080    --rotate normal \
-       --output VGA-0    --mode 1920x1080 --pos 0x0       --rotate normal \
-       --output HDMI-1-2 --off \
+xrandr --output HDMI-0   --mode 1920x1080 --pos 1920x1080 --rotate normal --auto --primary \
+       --output HDMI-1-2 --mode 1920x1080 --pos 0x1080    --rotate normal --auto \
+       --output VGA-0    --mode 1920x1080 --pos 0x0       --rotate normal --auto \
+       --output HDMI-1-3 --off \
        --output DVI-D-0  --off \
        --output DP-1-1   --off
