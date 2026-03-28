efc1c4f207f4692cb0aa6b6a27bbbe98a852e3fe
Albert Gil <albert.gil@happycomputing.cat>
2026-03-28 13:12:29 +0100

    Fix suspend/resume issue

--- .screenlayout/3monitors-new.sh
+++ .screenlayout/3monitors-new.sh
@@ -1,7 +1,2 @@
 #!/bin/sh
-xrandr --output HDMI-0   --mode 1920x1080 --pos 1920x1080 --rotate normal --auto --primary \
-       --output HDMI-1-2 --mode 1920x1080 --pos 0x1080    --rotate normal --auto \
-       --output VGA-0    --mode 1920x1080 --pos 0x0       --rotate normal --auto \
-       --output HDMI-1-3 --off \
-       --output DVI-D-0  --off \
-       --output DP-1-1   --off
+xrandr --output HDMI-0 --mode 1920x1080 --pos 0x1080 --rotate normal --output DP-0 --off --output DP-1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-2 --off --output DP-3 --mode 1920x1080 --pos 1920x1080 --rotate normal --output DP-4 --off --output DP-5 --off --output HDMI-A-1-0 --off --output DisplayPort-1-0 --off --output DisplayPort-1-1 --off
